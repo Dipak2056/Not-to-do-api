2341c2a2454f7e9f1d19ebd5cae90c57c7c04b11
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-12 09:08:54 +1000

    added and received data

--- server.js
+++ server.js
@@ -2,11 +2,29 @@
 import express from "express";
 const app = express();
 const PORT = 8000;
-
+const fakeTasks = [];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //Api end points
-app.get("/api/v1/", (req, res) => {
+//task api end points
+app.get("/api/v1/task", (req, res) => {
+  //replace the tasks with the real ones from databases
+
+  res.json({ message: "you made a get call", tasks: fakeTasks });
+});
+
+app.post("/api/v1/task", (req, res) => {
+  fakeTasks.push(req.body);
+  console.log(req.body);
   res.json({
-    message: "welcome to the api",
+    status: "Success",
+    message: "you made a post call",
+  });
+});
+
+app.delete("/api/v1/task", (req, res) => {
+  res.json({
+    message: "you made a delete call",
   });
 });
 
