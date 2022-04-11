b79594578b3c3343d048ef116feffe5cee1f39eb
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-11 15:02:11 +1000

    making api call

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+//Api end points
+app.get("/api/v1/", (req, res) => {
+  res.json({
+    message: "welcome to the api",
+  });
+});
+
+app.use("/", (request, response) => {
+  const person = {
+    name: "dipak",
+    lastname: "paudel",
+  };
+  response.json({ message: "you have reached to the api of not to do app" });
+});
+
+app.listen(PORT, (error) =>
+  error
+    ? console.log(error)
+    : console.log(`server is running at http://localhost:${PORT}`)
+);
