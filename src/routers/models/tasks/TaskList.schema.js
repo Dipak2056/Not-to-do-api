c23ab27293d5f9595f931b91f19efd3d19668c0b
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-12 14:36:45 +1000

    CREATED SCHEMA

---
+++ src/routers/models/tasks/TaskList.schema.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const TaskListSchema = new mongoose.Schema({
+  task: { type: String, required: true, default: "", min: 3, max: 30 },
+  hr: { type: Number, required: true },
+});
