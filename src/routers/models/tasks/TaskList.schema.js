f817afcab46b09a18c70c70cdfdcdd89fa17046e
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-12 14:38:05 +1000

    model has been added

--- src/routers/models/tasks/TaskList.schema.js
+++ src/routers/models/tasks/TaskList.schema.js
@@ -4,3 +4,5 @@ const TaskListSchema = new mongoose.Schema({
   task: { type: String, required: true, default: "", min: 3, max: 30 },
   hr: { type: Number, required: true },
 });
+
+mongoose.model(Task, TaskListSchema);
